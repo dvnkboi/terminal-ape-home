5b8fafb4b676e279095bfafa14e5aaf92c32a7d0
dvnkboi <aymandb@outlook.com>
2022-04-09 15:32:07 +0000

    v0.1 terminalApe home

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:ital,wght@0,400;0,700;1,400;1,700&display=swap");
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
